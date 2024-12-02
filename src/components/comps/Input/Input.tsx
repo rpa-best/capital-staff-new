405ed327ad0f9a39bc93e8337a0836bb858fe0cc
KaRRd1 <vanyglory10@gmail.com>
2024-12-02 12:33:24 +0300

    Update Input

--- src/components/comps/Input/Input.tsx
+++ src/components/comps/Input/Input.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import style from "./Input.module.scss"
 
-interface IInput {
+interface IInput extends React.InputHTMLAttributes<HTMLInputElement> {
     id: string,
     label?: string,
     type: string,
     name: string,
-    value: any,
+    value?: any,
     placeholder?: string,
-    onChange: (event: any) => void
 }
+
 
 const Input = (props: IInput) => {
     return (
         <React.Fragment>
             <div>
                 <label className={style.label} htmlFor={props.id}><b>{props.label}</b></label>
-                <input className={style.input} type={props.type} id={props.id} name={props.name} placeholder={props.placeholder} onChange={(event) => props.onChange(event)} value={props.value}/>
+                <input className={style.input}
+                       type={props.type}
+                       id={props.id}
+                       name={props.name}
+                       placeholder={props.placeholder}
+                       onChange={(event) => props.onChange?.(event)}
+                       value={props.value}
+                />
             </div>
         </React.Fragment>
     )
