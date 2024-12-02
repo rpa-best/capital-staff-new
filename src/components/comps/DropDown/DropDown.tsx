1bee106dcea9869bec743b07f71edc4b62d69028
KaRRd1 <vanyglory10@gmail.com>
2024-12-02 12:33:08 +0300

    Update DropDown

--- src/components/comps/DropDown/DropDown.tsx
+++ src/components/comps/DropDown/DropDown.tsx
@@ -1,22 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 import scss from "./DropDown.module.scss"
 
 interface IValue {
     value: any,
     label: string
 }
+
 interface IDropDown {
     name: string,
     id: string,
     placeholder: string
     onChange: (event: any) => void
     values: IValue[]
+    value?: any,
+    required?: boolean
 }
-const DropDown = ({name, id, onChange, values, placeholder}: IDropDown) => {
+
+const DropDown = ({name, id, onChange, values, value, placeholder, required}: IDropDown) => {
+    const [selectedValue, setSelectedValue] = useState(value);
+    
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedValue(event.target.value)
+        onChange(event)
+    }
+    
     return (
         <React.Fragment>
-            <select className={scss.dropDown} name={name} id={id} onChange={(event) => onChange(event)}>
-                <option value={'-1'}>{placeholder}</option>
+            <select
+                id={id}
+                name={name}
+                className={scss.dropDown}
+                value={selectedValue}
+                onChange={handleChange}
+            >
+
+                <option value={'-1'} disabled={required}>{placeholder}</option>
+                
                 {values.map((tempValue) =>
                     <option value={tempValue.value}>{tempValue.label}</option>
                 )}
